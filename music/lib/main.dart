73015f421f587df7355fdf241f211c8b65f052ef
Samira Ahmadnezhad <134923629+SamiraAhmadnezhad@users.noreply.github.com>
2025-05-06 20:11:34 +0330

    Update main.dart

--- music/lib/main.dart
+++ music/lib/main.dart
@@ -8,16 +8,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Music App', // میتونید عنوان رو تغییر بدید
+      title: 'Music App',
       theme: ThemeData(
-        primarySwatch: Colors.blue, // میتونید تم رو تغییر بدید
+        primarySwatch: Colors.blue, 
       ),
-      // اینجا MusicPage رو به عنوان صفحه اصلی تنظیم می‌کنیم
-      home: const MusicPage(), // <--- اینجا تغییر اصلی هست
+      
+      home: const MusicPage(), 
     );
   }
 }
